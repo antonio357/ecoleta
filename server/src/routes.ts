7ab465a7273902d0060189a67a3fb4bfffae5292
antonio357 <carlosalbuquerque359@gmail.com>
2020-06-03 03:04:56 -0300

    recycable garbage list route made

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,11 @@
 import express from "express"
+import knex from './database/connection' // connecting the database with this route
 
 const routes = express.Router() // disattach the functionalities from the server.ts
 
-routes.get("/", (request, response) => {
-    return response.json({message: "Hello World"})
+routes.get("/garbage", async (request, response) => {
+    const garbages = await knex('garbage').select('*')
+    return response.json(garbages)
 })
 
 export default routes // so you can import to server.ts
