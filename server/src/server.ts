245d98f3eeb4400d283cea052168ea98de36c14b
antonio357 <carlosalbuquerque359@gmail.com>
2020-06-03 02:30:54 -0300

    route that serve an stact image file that will be used for making the logos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
-import express, { request, response, json } from "express" // deals with routes
+import express, { response, request } from "express" // deals with routes
 import routes from './routes'
+import path from 'path'
 
 const app = express()
 
@@ -11,5 +12,8 @@ it can interpret
 app.use(express.json())
 
 app.use(routes) // add routes to app
+
+// express.static serve static files (files from the aplication it self) like images, pdf
+app.use("/images", express.static(path.resolve(__dirname, '..', 'images')))
 
 app.listen(3333) // selecting the port
