b2021a65169e03706934380ba0a0fd761ece5123
antonio357 <carlosalbuquerque359@gmail.com>
2020-06-03 03:22:46 -0300

    recycable garbage list route serialized

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,19 @@
-import express from "express"
-import knex from './database/connection' // connecting the database with this route
+import express from "express";
+import knex from "./database/connection"; // connecting the database with this route
 
-const routes = express.Router() // disattach the functionalities from the server.ts
+const routes = express.Router(); // disattach the functionalities from the server.ts
 
 routes.get("/garbage", async (request, response) => {
-    const garbages = await knex('garbage').select('*')
-    return response.json(garbages)
-})
+    const garbage = await knex("garbage").select("*");
 
-export default routes // so you can import to server.ts
+    // serialized data means that the front-end will recive the data not in the original way from the database 'rawdata' but instead the data that will be send will be filtered
+    const serializedGarbage = garbage.map((garbage) => {
+        return {
+            classification: garbage.classification,
+            image_url: `http://localhost:3333/images/${garbage.image}`,
+        };
+    });
+    return response.json(serializedGarbage);
+});
+
+export default routes; // so you can import to server.ts
