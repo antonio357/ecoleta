6c7f9977bbb0f8dc2d616ce4ac28ba6e27155741
antonio357 <carlosalbuquerque359@gmail.com>
2020-06-04 15:25:23 -0300

    register page {add map, add items from pai with axios request}

--- web/src/pages/CreateLocation/index.tsx
+++ web/src/pages/CreateLocation/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import './styles.css'
 import {FiArrowLeft} from 'react-icons/fi'
+import {Map, TileLayer, Marker} from 'react-leaflet'
+import api from '../../services/api'
 
 import logo from '../../assets/logo.svg'
 
+// in useStates array or object: you need to manually define the type
+interface Item {
+    id: number
+    classification: string
+    image_url: string
+}
+
 const CreateLocation = () => {
+    const [items, setItems] = useState<Item[]>([])
+
+    useEffect(() => {
+        api.get('garbage')
+        .then(response => {
+            setItems(response.data)
+        })
+    }, [])
+
     return (
         <div id="page-create-point">
             <header>
@@ -59,6 +77,14 @@ const CreateLocation = () => {
                         <h2>Endereço</h2>
                         <span>Selecione um Endereço no Mapa</span>
                     </legend>
+
+                    <Map center={[-7.2183397,-35.9127367]} zoom={15}>
+                        <TileLayer 
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-7.2183397,-35.9127367]}/>
+                    </Map>
                     <div className="field-group">
                         <div className='field'>
                             <label htmlFor="uf">Estado (UF)</label>
@@ -82,30 +108,13 @@ const CreateLocation = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/images/oils.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oils.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oils.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oils.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oils.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/images/oils.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt=""/>
+                                <span>{item.classification}</span>
+                            </li>
+                        ))}
+                        
                     </ul>
                 </fieldset>
 
