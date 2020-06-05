3d8863e8b16493be763ac3a0dc2bfa0b4d637337
antonio357 <carlosalbuquerque359@gmail.com>
2020-06-05 16:26:24 -0300

    creating mobile home page {loading fonts and logo add}

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from "expo";
+import {StatusBar} from 'react-native';
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
+
+import Home from './src/pages/Home'
 
 // here it does not have html tags, only react-native elements
-
 // style={styles.container} this is how you add style
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ // this is made to load the fonts before the aplication page start
+    Roboto_400Regular, Roboto_500Medium, Ubuntu_700Bold
+  })
+
+  if (!fontsLoaded) return <AppLoading/> // th page will wait until fonts get loaded
+
+  // backgroundColor='transparent' translucent it is just for android
   return (
-    <View style={styles.container}> 
-      <Text style={styles.text}>Hello world</Text>
-    </View>
+    <>  
+      <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent/>
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  // obs there is no inheritance neither cascading each element has its own style
-  container: { // this is javascript object that will be translated to native style ios and android, the tool that does it is yoga from facebook
-    // here every style has display: 'flex' as default
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: '#FFF',
-  },
-});
