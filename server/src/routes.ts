cee462a3be5cef2439b0575b007a604338249408
antonio357 <carlosalbuquerque359@gmail.com>
2020-06-06 19:54:43 -0300

    server validating input from location data

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import GarbagesController from './controllers/GarbagesController'
 import LocationsController from './controllers/LocationsController'
+import {celebrate, Joi} from 'celebrate'
 
 import multer from "multer";
 import multerConfig from "./config/multer";
@@ -16,7 +17,22 @@ routes.get('/locations/:id', locationsController.show)
 routes.get('/locations', locationsController.index)
 
 
-routes.post("/locations", upload.single('image_url'), locationsController.create)
+routes.post(
+    "/locations", 
+    upload.single('image_url'), 
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            state_or_province: Joi.string().required().max(2),
+            garbages: Joi.string().required()
+        },)
+    }, {abortEarly: false}),
+    locationsController.create)
 
 
 export default routes; // so you can import to server.ts
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express, { response, request } from "express" // deals with routes
 import routes from './routes'
 import path from 'path'
 import cors from 'cors'
+import { errors } from "celebrate";
 
 const app = express()
 
@@ -18,5 +19,7 @@ app.use(routes) // add routes to app
 
 // express.static serve static files (files from the aplication it self) like images, pdf
 app.use("/images", express.static(path.resolve(__dirname, '..', 'images')))
+
+app.use(errors())
 
 app.listen(3333) // selecting the port
